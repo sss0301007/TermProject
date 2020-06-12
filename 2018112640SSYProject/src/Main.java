6c9b7c7d091adf29007c4efd69d2f546f012486d
sss0301007 <57606436+sss0301007@users.noreply.github.com>
2020-06-12 21:03:25 +0900

    두번째
    점수생성 / 배열 생성 완료

--- 2018112640SSYProject/src/Main.java
+++ 2018112640SSYProject/src/Main.java
@@ -1,9 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
+	
+	 static int [] users;
+   
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//
+	
+		 Scanner sc = new Scanner(System.in);
+    	 int n = 0;  	 
+    	 users = new int [n];
+    	 
+    	 generateScores(n);
+    	 for(int i=0; i<users.length; i++) {
+    		 System.out.println(users[i]);
+    	 }
+ 
 	}
 
+	 private static void generateScores(int num){
+
+	        Random random = new Random();
+
+	        for(int i =0 ;i<num ; i++){
+	            double x =random.nextDouble();
+	            users[i] = (int) (1000000 + 9000000 * x * x);
+	           
+	        }
+	    }
+	
 }
+
+
