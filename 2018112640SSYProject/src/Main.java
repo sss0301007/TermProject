211cba1ba4b71b386e3fbb949c8b0e7c5156a3e0
sss0301007 <57606436+sss0301007@users.noreply.github.com>
2020-06-22 03:57:45 +0900

    다섯번째_최종코드
    1. 예외처리 추가
    2. Errors check추가

--- 2018112640SSYProject/src/Main.java
+++ 2018112640SSYProject/src/Main.java
@@ -1,40 +1,64 @@
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 import javax.print.DocFlavor.STRING;
 
 public class Main {
+	static int [] users;
+	static int [] temp;
 
-	
+	 
 	public static void main(String[] args) {
 		// 랜덤score을 저장할 배열 생성
 		// 하나는 ssysort, 하나는 javasort로 정렬
-		int [] users;
-		int [] temp;
-		
+		Scanner sc = new Scanner(System.in);
 		// 배열의 크기인 n을 입력받음
-		 Scanner sc = new Scanner(System.in);
-    	 int n = 0;  	
-    	 n = sc.nextInt();
+    	 int n = 0;  
+    	 try {
+    		n = sc.nextInt();
+    	 }catch (InputMismatchException e) {
+    		 System.out.println("잘못된 값을 입력했습니다. 0보다 큰 정수를 입력하세요");
+    		 return;
+    	 }
+    	 
+    	 if(n<0) {
+ 	    	 System.out.println("0보다 작은 값을 입력했습니다.n0보다 큰 정수를 입력하세요");
+ 	    	 return;
+ 	     }
+    	 
     	 users = new int [n];
     	 temp = new int [n];
-    	 
-    	 //n만큼 random data 생성
+	
+    	 generateRandomData(n);
+    	 Sort.StartSSYsort(users);
+    	 Sort.StartjavaSort(temp);
+    	 checkErrors(users, temp);
+    	 }
+
+
+	
+	
+		//n만큼 random data 생성
+    	 public static void generateRandomData(int n) {
     	 Random random = new Random();
     	 for(int i =0 ;i<n; i++){
 	            double x =random.nextDouble();
 	            users[i] = (int) (1000000 + 9000000 * x * x);
 	            temp[i] = (int) (1000000 + 9000000 * x * x);
-	        }
-	  
-    	 
-    	//상위 30%의 k값을 찾을 수 있는지 실험
-    	 int k = Search.nth_element(users, n, (int) (users.length*0.3));
-    	 System.out.println(k);
-    	 System.out.println("usersArray");
-    	 for(int i=0; i<n; i++) {
-    		 System.out.println( "usersArray["+i+"]:" + users[i]);
     	 }
-	}
+	 }
+    	 
+    	  public static void checkErrors(int [] data1, int [] data2) {
+    		 int check = 0;
+    		 for(int i=0; i<(int)(data1.length*0.3-1); i++ ) {
+    			 if (data1[i] != data2[data2.length-1-i]) {
+    			check++; 
+    			 }
+    		}
+    		 System.out.println("Errors: "+ check);
+    	  }
+    	  
+    	 
 }
--- 2018112640SSYProject/src/Sort.java
+++ 2018112640SSYProject/src/Sort.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Random;
 
-public class SSYsort {
+public class Sort {
 	
 	//Start Javasort
 			public static void StartjavaSort(int[] data){
@@ -14,24 +14,25 @@ public class SSYsort {
 	//Start SSYsort
 			public static void StartSSYsort(int[] data) {
 					 long ckTime = System.currentTimeMillis();
-					SSYsort(data);
-					System.out.println("SSYsort: " + (System.currentTimeMillis() - ckTime)+"ms");
+					 SSYsort(data);
+					 System.out.println("SSYsort 소요시간: " + (System.currentTimeMillis() - ckTime)+"ms");
 						
 					}
 	
-	
-		public static void SSYsort(int[]data) {
+	//상위 30프로까지만 정렬하는 SSYsort
+		 public static void SSYsort(int[]data) {
 			//상위 30%에 위치한 index값 구하기 ex) lenght가 10인 배열의 상위 30%인덱스는 2
 			int pivotIndex = (int) (data.length*0.3-1);
 			//상위 30%인 K값 찾고, K보다 큰 값과 작은 값 분리  ex)lenght가 10인 배열에서 3번째로 큰 값 찾고, 그것을 기준으로 작은 값과 큰 값 분리
-			Search.nth_element(data, data.length, pivotIndex+1);
+			int pivotValue = Search.nth_element(data, data.length, pivotIndex+1);
 			//K보다 큰 값만 quicksort진행
+			System.out.println("상위  30% Index: " + pivotIndex +", " + "상위 30% k값: " + pivotValue);
 			quickSort(data, 0, pivotIndex);
 			
 		}
 			
 		//quicksort	- 출처 인터넷
-			public static void quickSort(int[] a, int left, int right) {
+		 public static void quickSort(int[] a, int left, int right) {
 			        int pl=left;
 			        int pr = right;
 			        int x = a[(pl+pr)/2];
@@ -54,15 +55,14 @@ public class SSYsort {
 			        a[idx2] = t;
 			    }
 			
-			
+			    //정렬된 배열을 확인하는 메소드
+			    public static void printArray(String arrayName, int[] array){
+			        System.out.println("print Array");
+			        for(int i =0 ; i<array.length ; i++){
+			            System.out.println(arrayName + " Array["+i+"] : " + array[i]);
+			        }
+			    }
 
 
 
 }
-		
-	
-
-
-
-	
-
